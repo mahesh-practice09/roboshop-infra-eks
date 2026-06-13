f9ff47c96c70f1cee2d41116f9b6e32c85604053
mahesh <mahesh050917@gmail.com>
2026-06-13 12:11:30 +0530

    roboshop eks infra code

---
+++ 31-cicd-tools/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z04311461UOYT2X1S4LOM"
+}
+
+variable "domain_name" {
+    default = "daws88s.online"
+}
+
+variable "sonar" {
+    default = true
+}
+
+variable "jenkins" {
+    default = true
+}
+
+variable "runner" {
+    default = false
+}
